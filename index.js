0df7caaf7e22fa20fb6be754b0506cfd961a08c4
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-09-24 15:22:56 +0530

    Added html

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+
+
+function checkBirthdateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+}
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum =0;
+    for (let i = 0; i<dob.length;i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+}
+
+
+
+checkNumberButton.addEventListener('click', checkBirthdateIsLucky);
